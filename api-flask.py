b758ad21a0a1a70eeab80da032f6ee10b5157ea7
stephenbpeters <stephen.peters@gmail.com>
2021-06-03 02:00:30 -0700

    flask API complete

--- api-flask.py
+++ api-flask.py
@@ -17,6 +17,7 @@ from sqlalchemy import inspect
 
 # create engine to hawaii.sqlite
 #engine = create_engine("sqlite:///Resouces/hawaii.sqlite")
+# refuses to read the file unless it's in the same dir, for some reason
 engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
@@ -35,7 +36,7 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-print(' -- let\'s look at some of our data from Measurement')
+#print(' -- let\'s look at some of our data from Measurement')
 measurement_df = pd.read_sql_query("select date, prcp, station from measurement where (date like '2017%')\
                                   or (date like '2016-09%')\
                                   or (date like '2016-10%')\
@@ -50,9 +51,8 @@ measurements_flask.set_index('date', inplace=True)
 #print(measurements_flask.head())
 measurements_data = measurements_flask.to_dict()
 
-print('Now getting our TOBS data for one year from station USC00519281')
+#print('Now getting our TOBS data for one year from station USC00519281')
 tobs_df = measurement_df[measurement_df.station != "USC00519281" ]
-
 
 #print('our TOBS dataframe is...')
 tobs_df.dropna(axis = 0, how = 'any', inplace=True)
@@ -61,12 +61,13 @@ tobs_data.set_index('date', inplace=True)
 tobs_flask = tobs_data.to_dict()
 #print(tobs_data.head())
 
-# import station data
-# test importing the station csv
-stations_df = pd.read_csv('Resources/station_data.csv')
-stations_df = stations_df.drop(columns = ['id', 'Unnamed: 0'])
-stations_data = stations_df.to_dict()
+temps_df = pd.read_sql_query("select date, tobs from measurement", engine)
+#print(temps_df)
 
+# import station data
+stations_df = pd.read_sql_query("SELECT * FROM station", engine)
+#print(stations_df)
+stations_data = stations_df.to_dict()
 
 # setup our Flask app
 app = Flask(__name__)
@@ -77,21 +78,29 @@ def home():
   print("server received request for 'home' page...")
   return (
       f"<blockquote>"
-      f"Welcome to Flask API home page, kind visitor<br/>"
+      f"<h2>Welcome to Hawaii Weather API home page</h2><br/>"
       f"<b>Available Routes:</b><br/>"
       f"<ul>"
-      f"<li><a href='/api/v1.0/precipitation' target='_blank'>/api/v1.0/precipitation</a>  <br> -- Return the JSON representation of your dictionary."
-      f"<br> -- Convert the query results to a dictionary using `date` as the key and `prcp` as the value.  <b>DONE!</b>"
-      f"<li><a href='/api/v1.0/stations' target='_blank'>/api/v1.0/stations</a> <br> -- Return a JSON list of stations from the dataset. <b>DONE!</b>"
+      f"<li><a href='/api/v1.0/precipitation' target='_blank'>/api/v1.0/precipitation</a>  <br> -- Returns date and precipitation observation for every record in our data."
+
+      f"<li><a href='/api/v1.0/stations' target='_blank'>/api/v1.0/stations</a> <br> -- Returns all the data about our observation stations"
+      
       f"<li><a href='/api/v1.0/tobs' target='_blank'>/api/v1.0/tobs</a>"
-      f"<br> -- Query the dates and temperature observations of the most active station for the last year of data."
-      f"<br> -- Return a JSON list of temperature observations (TOBS) for the previous year.   <b>DONE!</b>"
-      f"<li><a href='/api/v1.0/test' target='_blank'>testing url</a>"     
-      f"<li>/api/v1.0/&ltstart&gt and /api/v1.0/&ltstart&gt/&ltend&gt"
-      f"<br> -- Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range."
-      f"<br> -- When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date."
-      f"<br> -- When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive."
-      f"<li><a href='/api/v1.0/justice-league/real_name/ target='_blank'>/api/v1.0/justice-league/real_name/</a> add real name and get back super name <b>DONE REMOVE!</B>"
+      f"<br> -- Returns the dates and temperature observations of the most active station for the last twelve months of data."
+      
+      f"<li>/api/v1.0/&ltstart date&gt"
+      f"<br> -- Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date through the last date on record."
+      f"<br> -- Date format needs to be YYYY-MM-DD."
+      
+      f"<li>/api/v1.0/&ltstart date&gt/&ltend date&gt"      
+      f"<br> -- Returns a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date through the end date."
+      f"<br> -- Date format needs to be YYYY-MM-DD."     
+       
+      f"<li>/human-readable/&ltstart date&gt"
+      f"<br> -- same thing as above, only better formatted for reading"
+      
+      f"<li>/human-readable/&ltstart date&gt/&ltend date&gt"
+      f"<br> -- same thing as above, only better formatted for reading"      
       f"</ol>"
       f"</blockquote>"
     )
@@ -99,55 +108,107 @@ def home():
 @app.route("/api/v1.0/precipitation")
 def measurements():
     """Return the measurement data as json"""
-
+    print("server received request for precipitation data...")
     return jsonify(measurements_data)     
     
 @app.route("/api/v1.0/stations")
 def stations():
     """Return the stations data as json"""
-
+    print("server received request for stations data...")
     return jsonify(stations_data)    
 
-@app.route("/api/v1.0/test/<foo_var>/<foo2_var>")
-def test(foo_var, foo2_var):
-    """let us just test picking up those variables"""
-    insult = "nixnut"
-    print("server received request for about page...")
+@app.route("/api/v1.0/<start_date>")
+def zeetemps(start_date):
+    """here we get our aggregate stats for our date range"""
+    print("server received request for tobs stats start to end of data...")
+    # correct for dates before the start of our data
+    if start_date < '2010-01-01':
+      start_date = '2010-01-01'
+    # set end date
+    end_date = '2017-08-23'
+    range_df = temps_df[(temps_df['date'] >= start_date) & (temps_df['date'] <= end_date)]
+    lowest = range_df['tobs'].min()
+    highest = range_df['tobs'].max()
+    average = range_df['tobs'].mean()
+    output = {'TMIN': lowest, 'TMAX': highest, 'TAVG': average}
+    return jsonify(output)
+    
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end(start_date, end_date):
+    """here we get our aggregate stats for our date range"""
+    print("server received request for tobs stats start date to end date...")
+    # correct for dates before the start of our data
+    if start_date < '2010-01-01':
+      start_date = '2010-01-01'
+    # correct for dates beyond the end of our data
+    if end_date > '2017-08-23':
+      end_date = '2017-08-23'
+    range_df = temps_df[(temps_df['date'] >= start_date) & (temps_df['date'] <= end_date)]
+    lowest = range_df['tobs'].min()
+    highest = range_df['tobs'].max()
+    average = range_df['tobs'].mean()
+    output = {'TMIN': lowest, 'TMAX': highest, 'TAVG': average}
+    return jsonify(output)    
+
+@app.route("/human-readable/<start_date>")
+def temps(start_date):
+    """here we get our aggregate stats for our date range"""
+    print("server received request for human readable tobs stats start date to end date...")
+    # correct for dates before the start of our data
+    if start_date < '2010-01-01':
+      start_date = '2010-01-01'
+    # set end date
+    end_date = '2017-08-23'
+    range_df = temps_df[(temps_df['date'] >= start_date) & (temps_df['date'] <= end_date)]
+    lowest = range_df['tobs'].min()
+    highest = range_df['tobs'].max()
+    average = range_df['tobs'].mean()
+    observations = range_df['tobs'].count()
+    output = {'TMIN': lowest, 'TMAX': highest, 'TAVG': average}
     return (
-      f"Welcome to the test page, you {insult}.<br/>"
-      f"you typed... {foo_var}<br>"
-      f"...and you also typed... {foo2_var}<br>"
-      f"and your mother wears combat boots."
+      f"<blockquote><br><br>"
+      f"<h2>Thanks for checking the weather in Hawaii!</h2><br/>"
+      f"You asked for a <i>start date</i> of: <b>{start_date}</b><br>"
+      f"...and an <i>end date</i> of: <b>{end_date}</b><br><br>"
+      f"There are <b>{observations}</b> observation records in this date range.<br><br>"
+      f"The maximum observed temperature in that date range is: <b>{highest}</b><br>"
+      f"The minimum observed temperature in that date range is: <b>{lowest}</b><br>"
+      f"The average temperature in that date range is: <b>{round(average, 4)}</b><br>"
+      f"</blockquote>"
+      )
+    
+@app.route("/human-readable/<start_date>/<end_date>")
+def human_start_end(start_date, end_date):
+    """here we get our aggregate stats for our date range"""
+    print("server received request for human readable tobs stats start date to end date...")
+    # correct for dates before the start of our data
+    if start_date < '2010-01-01':
+      start_date = '2010-01-01'
+    # correct for dates beyond the end of our data
+    if end_date > '2017-08-23':
+      end_date = '2017-08-23'
+    range_df = temps_df[(temps_df['date'] >= start_date) & (temps_df['date'] <= end_date)]
+    lowest = range_df['tobs'].min()
+    highest = range_df['tobs'].max()
+    average = range_df['tobs'].mean()
+    observations = range_df['tobs'].count()
+    return (
+      f"<blockquote><br><br>"
+      f"<h2>Thanks for checking the weather in Hawaii!</h2><br/>"
+      f"You asked for a <i>start date</i> of: <b>{start_date}</b><br>"
+      f"...and an <i>end date</i> of: <b>{end_date}</b><br><br>"
+      f"There are <b>{observations}</b> observation records in this date range.<br><br>"      
+      f"The maximum observed temperature in that date range is: <b>{highest}</b><br>"
+      f"The minimum observed temperature in that date range is: <b>{lowest}</b><br>"
+      f"The average temperature in that date range is: <b>{round(average, 4)}</b><br>"
+      f"</blockquote>"
       )
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Return the TOBS data of station USC00519281 from the last 12 months as json"""
-
+    print("server received request for the last year of TOBS data...")
     return jsonify(tobs_flask)    
-    
-@app.route("/api/v1.0/justice-league/real_name/<real_name>")
-def justice_league_by_real_name(real_name):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
-
-    canonicalized = real_name.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower()
-
-        if search_term == canonicalized:
-            return jsonify(character)
-
-    return jsonify({"sorry": f"Character with real_name {real_name} not found."}), 404    
-  
-@app.route("/about")  
-def about():
-  insult = "nixnut"
-  print("server received request for about page...")
-  return (
-    f"Welcome to ABOUT page, you {insult}.<br/>"
-    f"and your mother wears combat boots."
-    )
   
 if __name__ == "__main__":
   app.run(debug=True)  
